391e7bae6e031c5fcb8bb575277160a0e6ca4705
Haruko <haruko.flipsy.krb@gmail.com>
2020-09-12 21:56:48 +0900

    first commit

---
+++ Memopad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memopad
+//
+//  Created by Haruko Okada on 9/3/20.
+//  Copyright © 2020 Haruko Okada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
